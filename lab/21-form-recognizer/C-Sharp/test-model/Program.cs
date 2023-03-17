378a445eef45c18adf78498aa2ad6cf0a3e061a7
TzuYi Chao <tzuyichao0313@gmail.com>
2023-03-17 14:18:39 +0800

    Custom Form Recognition

---
+++ lab/21-form-recognizer/C-Sharp/test-model/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+// import namespaces
+using Azure;
+using Azure.AI.FormRecognizer;  
+using Azure.AI.FormRecognizer.Models;
+using Azure.AI.FormRecognizer.Training;
+
+namespace test_mode
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            try
+            {
+                IConfigurationBuilder builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json");
+                IConfigurationRoot configuration = builder.Build();
+                string formEndpoint = configuration["FormEndpoint"];
+                string formKey = configuration["FormKey"];
+                string modelId = configuration["ModelId"];
+
+                var credential = new AzureKeyCredential(formKey);
+                var recognizerClient = new FormRecognizerClient(new Uri(formEndpoint), credential);
+
+                string imageFile = "test1.jpg";
+                using(var imageData = File.OpenRead(imageFile))
+                {
+                    RecognizedFormCollection forms = await recognizerClient.StartRecognizeCustomForms(modelId, imageData)
+                        .WaitForCompletionAsync();
+                    foreach(RecognizedForm form in forms)
+                    {
+                        Console.WriteLine($"Form of type: {form.FormType}");
+                        foreach(FormField field in form.Fields.Values)
+                        {
+                            Console.WriteLine($"Field '{field.Name}: ");
+
+                            if (field.LabelData != null)
+                            {
+                                Console.WriteLine($"    Label: '{field.LabelData.Text}");
+                            }
+
+                            Console.WriteLine($"    Value: '{field.ValueData.Text}");
+                            Console.WriteLine($"    Confidence: '{field.Confidence}");
+                        }
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ lab/21-form-recognizer/C-Sharp/train-model/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+// import namespaces
+using Azure;
+using Azure.AI.FormRecognizer;  
+using Azure.AI.FormRecognizer.Models;
+using Azure.AI.FormRecognizer.Training;
+
+namespace train_model
+{
+    class Program
+    {
+        static async Task Main() 
+        {
+            try
+            {
+                IConfigurationBuilder builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json");
+                IConfigurationRoot configuration = builder.Build();
+                string formEndpoint = configuration["FormEndpoint"];
+                string formKey = configuration["FormKey"];
+                string trainingStorageUri = configuration["StorageUri"];
+
+                var credential = new AzureKeyCredential(formKey);
+                var trainingClient = new FormTrainingClient(new Uri(formEndpoint), credential);
+
+                CustomFormModel model = await trainingClient.StartTrainingAsync(new Uri(trainingStorageUri), useTrainingLabels: true)
+                    .WaitForCompletionAsync();
+                
+                Console.WriteLine($"Custom Model Info:");
+                Console.WriteLine($"    Model Id: {model.ModelId}");
+                Console.WriteLine($"    Model Status: {model.Status}");
+                Console.WriteLine($"    Training model started on: {model.TrainingStartedOn}");
+                Console.WriteLine($"    Training model completed on: {model.TrainingCompletedOn}");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
