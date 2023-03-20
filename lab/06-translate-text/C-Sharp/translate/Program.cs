3fb7ee097e0b395e791ebcd69854f99479a3b487
TzuYi Chao <tzuyichao0313@gmail.com>
2023-03-20 17:20:11 +0800

    partial

--- lab/06-translate-text/C-Sharp/translate/Program.cs
+++ lab/06-translate-text/C-Sharp/translate/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Net.Http;
+using Microsoft.Extensions.Configuration;
+using System.Threading.Tasks;
+
+namespace translate_text
+{
+    class Program
+    {
+        static string translateEndpoint = "https://api.cognitive.microsofttranslator.com";
+        static string cogSvcKey;
+        static string cogSvcRegion;
+        static async Task Main()
+        {
+
+        }
+    }
+}
